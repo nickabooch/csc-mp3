dd65072658403c88e4fa62f69ba6db03a2a01180
booch <98662531+nickabooch@users.noreply.github.com>
2024-02-14 22:47:37 -0600

    These are the first bit of files

---
+++ RightJustified.java
@@ -0,0 +1,65 @@
+
+/**
+ * A text block that right-justifies its contents within a given width.
+ * Throws an exception if the provided width is less than the width of the text block.
+ * Implements the TextBlock interface for compatibility with the text layout system.
+ * 
+ * @author Samuel A. Rebelsky
+ * @author Rommin Adl
+ * @author Shibam Mukhopadhyay
+ */
+public class RightJustified implements TextBlock {
+    // +--------+------------------------------------------------------------
+    // | Fields |
+    // +--------+
+    private TextBlock textBlock;
+    private int maxWidth;
+
+    // +-------------+-------------------------------------------------------
+    // | Constructor |
+    // +-------------+
+    /**
+     * Constructs a RightJustified text block with the specified text block and maximum width.
+     * 
+     * @param textBlock The text block to be right-justified.
+     * @param maxWidth The width within which the text block should be right-justified.
+     * @throws IllegalArgumentException if the maxWidth is less than the width of the text block.
+     */
+    public RightJustified(TextBlock textBlock, int maxWidth) {
+        if (textBlock.width() > maxWidth) {
+            throw new IllegalArgumentException("Provided width must be longer than or equal to the block's width.");
+        }
+        this.textBlock = textBlock;
+        this.maxWidth = maxWidth;
+    }
+
+    // +---------+-----------------------------------------------------------
+    // | Methods |
+    // +---------+
+    @Override
+    public String row(int i) throws Exception {
+        if (i < 0 || i >= this.height()) {
+            throw new IllegalArgumentException("Invalid row " + i);
+        }
+        String originalRow = this.textBlock.row(i);
+        int paddingLength = this.maxWidth - originalRow.length();
+        return TBUtils.spaces(paddingLength) + originalRow;
+    }
+
+    @Override
+    public int height() {
+        return this.textBlock.height();
+    }
+
+    @Override
+    public int width() {
+        return this.maxWidth;
+    }
+
+    @Override
+    public boolean eqv(TextBlock other) {
+        if (!(other instanceof RightJustified)) return false;
+        RightJustified otherRightJustified = (RightJustified) other;
+        return this.textBlock.eqv(otherRightJustified.textBlock) && this.maxWidth == otherRightJustified.maxWidth;
+    }
+}
---
+++ VerticallyFlipped.java
@@ -0,0 +1,67 @@
+/**
+ * Vertically flips a TextBlock.
+ * 
+ * @author Rommin Adl
+ * @author Shibam Mukhopadhyay
+ */
+public class VerticallyFlipped implements TextBlock {
+    // Represents the unflipped block
+    private TextBlock inputBlock;
+
+    /**
+     * Construct a vertically flipped version of the input TextBlock.
+     * 
+     * @param inputBlock The TextBlock to be flipped.
+     */
+    public VerticallyFlipped(TextBlock inputBlock) {
+        this.inputBlock = inputBlock;
+    }
+
+    /**
+     * Get one row from the block, reversed.
+     * 
+     * @param i The row index to get, starting from 0 for the bottom row.
+     * @return The i-th row from the bottom.
+     * @throws Exception if the index is out of bounds.
+     */
+    @Override
+    public String row(int i) throws Exception {
+        // Ensure the row index is valid
+        if (i < 0 || i >= this.height()) {
+            throw new IllegalArgumentException("Row index is out of bounds.");
+        }
+        // Flip vertically by accessing rows from the bottom up
+        return inputBlock.row(inputBlock.height() - 1 - i);
+    }
+
+    /**
+     * Determine how many rows are in the block.
+     * 
+     * @return The height of the block.
+     */
+    @Override
+    public int height() {
+        return this.inputBlock.height();
+    }
+
+    /**
+     * Determine how many columns are in the block.
+     * 
+     * @return The width of the block.
+     */
+    @Override
+    public int width() {
+        return this.inputBlock.width();
+    }
+
+    /**
+     * Determine if two TextBlocks are equivalent.
+     * 
+     * @param other Another TextBlock to compare with this one.
+     * @return true if the other TextBlock is a VerticallyFlipped instance with an equivalent inputBlock; false otherwise.
+     */
+    public boolean eqv(TextBlock other) {
+        return (other instanceof VerticallyFlipped) && this.inputBlock.eqv(((VerticallyFlipped) other).inputBlock);
+    }
+
+}
